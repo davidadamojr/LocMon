2e551314fc662de33d675616dd2f770d4bd5efd6
David Adamo Jr <me@davidadamojr.com>
2015-03-25 20:22:08 -0500

    completed database creation class, manifest file and created MainActivity

---
+++ app/src/main/java/edu/unt/sell/contextmon/db/BroadcastDatabaseHelper.java
@@ -0,0 +1,11 @@
+package edu.unt.sell.contextmon.db;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by SELL-1 on 3/25/2015.
+ */
+public class BroadcastDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "broadcast_table.db";
+    private static final int DATABASE_VERSION = 1;
+}
---
+++ app/src/main/java/edu/unt/sell/contextmon/db/BroadcastTable.java
@@ -0,0 +1,33 @@
+package edu.unt.sell.contextmon.db;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * Created by SELL-1 on 3/25/2015.
+ */
+public class BroadcastTable {
+
+    // Database table
+    public static final String TABLE_NAME = "broadcasts";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_ACTION = "action";
+    public static final String COLUMN_EXTRAS = "extras";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+    // Database creation SQL statement
+    private static final String DATABASE_CREATE = "create table " + TABLE_NAME + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_ACTION + " text not null, " +
+            COLUMN_EXTRAS + " text not null," + COLUMN_TIMESTAMP + " text not null" + ");";
+
+    public static void onCreate(SQLiteDatabase database){
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
+        Log.w(BroadcastTable.class.getName(), "Upgrading database from version " + oldVersion +
+                " to " + newVersion + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(database);
+    }
+}
