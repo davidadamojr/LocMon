81f12e8e483a84b956a0ab965919972946f009ef
David Adamo <me@davidadamojr.com>
2015-03-28 16:27:47 -0500

    Completed ContentProvider for broadcasts table

--- app/src/main/java/edu/unt/sell/contextmon/db/BroadcastDatabaseHelper.java
+++ app/src/main/java/edu/unt/sell/contextmon/db/BroadcastDatabaseHelper.java
@@ -1,5 +1,7 @@
 package edu.unt.sell.contextmon.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -8,4 +10,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class BroadcastDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "broadcast_table.db";
     private static final int DATABASE_VERSION = 1;
+
+    public BroadcastDatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // this method is called during creation of the database
+    @Override
+    public void onCreate(SQLiteDatabase database){
+        BroadcastTable.onCreate(database);
+    }
+
+    // this method is called during an upgrade of the database
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
+        BroadcastTable.onUpgrade(database, oldVersion, newVersion);
+    }
 }
